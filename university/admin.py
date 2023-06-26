b57c24f82fb5ca62dc137915139d71afcc85ab5a
MohamadrezaPiri <piry.mreza@gmail.com>
2023-06-26 12:29:23 +0330

    Add inlines to StudentAdmin class

--- university/admin.py
+++ university/admin.py
@@ -13,3 +13,4 @@ class AddressInline(admin.TabularInline):
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     fields = ['first_name','last_name','gender','birth_date','grade','registration_date','graduation_date','phone']
+    inlines = [AddressInline]
