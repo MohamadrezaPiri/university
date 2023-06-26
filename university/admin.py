6fcd1306e3483eb62f861ea58b218fc349cefb08
MohamadrezaPiri <piry.mreza@gmail.com>
2023-06-26 12:28:42 +0330

    Create AddressInline Class in Admin.py

--- university/admin.py
+++ university/admin.py
@@ -3,6 +3,12 @@ from .models import Student, Address
 
 # Register your models here.
 
+class AddressInline(admin.TabularInline):
+    min_num = 1
+    max_num = 1
+    model = Address
+    extra = 0
+
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
