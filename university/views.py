12e2ea73ae8203d0de72eee6a36246fb15661578
MohamadrezaPiri <piry.mreza@gmail.com>
2023-06-26 16:17:47 +0330

    Add ordering_fields to StudentViewSet class

--- university/views.py
+++ university/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Student
 from .serializers import StudentSerializer
@@ -10,6 +10,7 @@ from .serializers import StudentSerializer
 class StudentViewset(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ['last_name']
+    ordering_fields = ['registration_date','graduation_date']
 
