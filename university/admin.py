5aa3902f51754a2eedeb65fb94d8557f2baacfb8
MohamadrezaPiri <piry.mreza@gmail.com>
2023-06-26 12:27:04 +0330

    Unregister Address model in admin.py file

--- university/admin.py
+++ university/admin.py
@@ -3,10 +3,7 @@ from .models import Student, Address
 
 # Register your models here.
 
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Address)
-class AddressAdmin(admin.ModelAdmin):
     pass
