734e052986e829bae90c8123523f1bbcb7a7016a
MohamadrezaPiri <piry.mreza@gmail.com>
2023-06-26 16:34:04 +0330

    Set list_per_page to 10 in StudentAdmin class

--- university/admin.py
+++ university/admin.py
@@ -8,3 +8,4 @@ class StudentAdmin(admin.ModelAdmin):
     fields = ['first_name','last_name','gender','birth_date','grade','registration_date','graduation_date','address','phone']
     list_display = ['first_name','last_name','birth_date','grade','registration_date','graduation_date','phone']
     list_filter = ['gender','grade']
+    list_per_page = 10
