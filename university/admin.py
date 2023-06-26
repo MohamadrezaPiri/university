59c9ba03ca286b842a6999688ec49d51718e3dde
MohamadrezaPiri <piry.mreza@gmail.com>
2023-06-26 14:43:15 +0330

    Register Student model in admin.py file

--- university/admin.py
+++ university/admin.py
@@ -3,3 +3,7 @@ from .models import Student
 
 # Register your models here.
 
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    fields = ['first_name','last_name','gender','birth_date','grade','registration_date','graduation_date','address','phone']
+    list_display = ['first_name','last_name','birth_date','grade','registration_date','graduation_date','phone']
