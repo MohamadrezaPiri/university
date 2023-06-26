3b832c97dd8c3a823b6261271d83d9f050004753
MohamadrezaPiri <piry.mreza@gmail.com>
2023-06-26 14:49:15 +0330

    Add gender & address to fields in StudentSerializer class

--- university/serializers.py
+++ university/serializers.py
@@ -4,5 +4,5 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['first_name','last_name','birth_date','grade','registration_date','graduation_date','phone']
+        fields = ['first_name','last_name','birth_date','gender','grade','registration_date','graduation_date','address','phone']
         
