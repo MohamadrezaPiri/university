506a543bc7676b04ed1703c5753817be4457d555
MohamadrezaPiri <piry.mreza@gmail.com>
2023-06-26 14:50:49 +0330

    Create StudentViewSet class in views.py

--- university/views.py
+++ university/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Student
+from .serializers import StudentSerializer
 
 # Create your views here.
+
+class StudentViewset(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
