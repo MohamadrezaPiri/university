0f3b9784d0ea0feef4b15a7e24b739d427979e67
MohamadrezaPiri <piry.mreza@gmail.com>
2023-06-26 14:56:26 +0330

    Add id to fields in StudentSerializer class

--- university/serializers.py
+++ university/serializers.py
@@ -4,5 +4,5 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['first_name','last_name','birth_date','gender','grade','registration_date','graduation_date','address','phone']
+        fields = ['id','first_name','last_name','birth_date','gender','grade','registration_date','graduation_date','address','phone']
         
